999f97ca4764ac8ddcec34394e7c972ba58fe136
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-04-10 17:43:44 +0200

    finishing touches

--- Datalagring_examination/Program.cs
+++ Datalagring_examination/Program.cs
@@ -1,25 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Datalagring_examination.Services;
 
-/*
- Kundens förnamn, kundens efternamn, kundens e-postadress, kundens telefonnummer, 
- beskrivning av ärendet, tidpunkt då ärendet skapades och en status på själva ärendet, 
- du kan behöva lägga till ytterligare information för att få allt att fungera. 
- Det är helt okej att mata in datat manuellt i databasen om man så vill göra det. 
- Så länge du kan få ut informationen och visa det i applikationen. 
-*/
+StatusService statusService = new();
 
-/* Det ska finnas en databasstruktur som är normaliserad i 1-3NF.
-Du måste minst ha två olika tabeller som har koppling till varandra.
-Det ska vara möjligt att skapa ett ärende och det ska lagras i databasen.
-Det ska vara möjligt att kunna se alla ärenden som finns inlagda i databasen.
-Det ska vara möjligt att kunna se ett specifikt ärende som finns inlagt i databasen.
-Det ska vara möjligt att byta status på ett ärende enligt statuslägena som finns ovan.
-*/
+await statusService.InitializeAsync();
 
-/*
- Du ska ha gjort det som specificeras upp under kraven för godkänt.
-Du måste minst ha fyra olika tabeller.
-Det ska vara möjligt att skriva en kommentar till ett ärende.
-Det ska vara möjligt att kunna se ett ärende och se kommentarer kopplat till ärendet.
-*/
+
+//Keeps service running in case user wants to continue after completing a task
+
+ProgramServices programServices = new();
+
+bool serviceRunning = true;
+
+while (serviceRunning == true) { 
+
+	await programServices.Main();
+
+	Console.WriteLine("\nÖnskar du avsluta servicen? \n 'J' för ja  / 'N' för nej");
+	var input = Console.ReadLine().ToUpper();
+
+	switch(input)
+	{
+		case "J":
+			serviceRunning = false;
+			break;
+
+		case "N":
+			serviceRunning = true;
+			break;
+
+		default:
+			Console.WriteLine("Ogiltigt svar");
+			Console.ReadLine();
+			break;
+	}
+
+	Console.Clear();
+}
