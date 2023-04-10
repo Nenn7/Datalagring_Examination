7f454113a9f1597fc0a42bf657f4ecd299167e49
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-04-10 17:43:01 +0200

    Finished customerservice

---
+++ Datalagring_examination/Services/CustomerService.cs
@@ -0,0 +1,36 @@
+﻿using Datalagring_examination.Contexts;
+using Datalagring_examination.Models;
+using Datalagring_examination.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Datalagring_examination.Services;
+
+internal class CustomerService
+{
+	private readonly DataContext _context = new DataContext();
+
+	public async Task<CustomerEntity> CreateAsync(CustomerModel model)
+	{
+		//Checks if a customer already exists based on email, if not, creates a new customer based on the information provided
+
+		var _customerEntity = await _context.Customers.FirstOrDefaultAsync(x => x.Email == model.Email);
+
+		if (_customerEntity == null)
+		{
+			_customerEntity = new CustomerEntity()
+			{
+				FirstName = model.FirstName,
+				LastName = model.LastName,
+				Email = model.Email,
+				PhoneNumber = model.PhoneNumber,
+			};
+
+			await _context.AddAsync(_customerEntity);
+			await _context.SaveChangesAsync();	
+		}
+
+		return _customerEntity;
+	}
+
+}
