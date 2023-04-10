c1b597d9dc06c8839ad6a06b9a0868d5c31c4aa9
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-04-10 17:42:25 +0200

    Finished commentservice

---
+++ Datalagring_examination/Services/CommentService.cs
@@ -0,0 +1,23 @@
+﻿using Datalagring_examination.Contexts;
+using Datalagring_examination.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Datalagring_examination.Services;
+
+internal class CommentService
+{
+	private readonly DataContext _context = new();
+
+	public async Task CreateAsync(CommentEntity comment)
+	{
+		//If any cases exists matching Id with comment caseId, creates a new comment
+
+		if(await _context.Cases.AnyAsync(x => x.Id == comment.CaseId))
+		{
+			_context.Add(comment);
+			await _context.SaveChangesAsync();
+		}
+	}
+
+}
