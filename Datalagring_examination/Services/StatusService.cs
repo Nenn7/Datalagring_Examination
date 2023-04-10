5d468168784382451e6601bccce498a4d0c2e319
Ellen Olsson <111755284+Nenn7@users.noreply.github.com>
2023-04-10 17:43:11 +0200

    finished statusservice

---
+++ Datalagring_examination/Services/StatusService.cs
@@ -0,0 +1,30 @@
+﻿using Datalagring_examination.Contexts;
+using Datalagring_examination.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Datalagring_examination.Services;
+
+internal class StatusService
+{
+	private readonly DataContext _context = new();
+
+	public async Task InitializeAsync()
+	{
+		//Creates three different statuses onto the status table upon calling the method
+
+		if (!await _context.Statuses.AnyAsync())
+		{
+			List<StatusEntity> list = new()
+			{
+				new StatusEntity() { Status = "Ej påbörjad" },
+				new StatusEntity() { Status = "Pågående" },
+				new StatusEntity() { Status = "Avslutad" },
+			};
+
+			_context.AddRange(list);
+			await _context.SaveChangesAsync();
+		}
+}
+
+}
